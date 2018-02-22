6ac621a70bc7d6a06b6453d72d3785d807d09d1e
Ali Jabir <syedalijabir@gmail.com>
2018-02-22 23:09:59 +0500

    Update code_simulation.m

--- Matlab/code_simulation.m
+++ Matlab/code_simulation.m
@@ -1,4 +1,6 @@
-% Copyright (c) 2018, Xgrid Inc, http://xgrid.co
+%
+% Owner: Ali Jabir
+% Email: syedalijabir@gmail.com
 %
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
